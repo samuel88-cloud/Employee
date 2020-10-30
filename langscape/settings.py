70d636f661b1becdff5383241cbd72c4c9075266
samuel stephen <samuel8stephen@gmail.com>
2020-10-30 19:14:43 +0530

    heroku configuration

--- langscape/settings.py
+++ langscape/settings.py
@@ -127,5 +127,16 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL='employee-list'
 LOGIN_URL = 'login'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
